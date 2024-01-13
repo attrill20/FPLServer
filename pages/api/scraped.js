5bd7f90e7cbafb5ba9983665456aa141c0795032
James Attrill <attrill20@gmail.com>
2024-01-13 16:20:48 +0000

    logic for scraped xG data correct

--- pages/api/scraped.js
+++ pages/api/scraped.js
@@ -19,30 +19,47 @@ export default async (req, res) => {
 async function scrapeData() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-
+  
   try {
     await page.goto('https://understat.com/league/EPL');
     await page.waitForSelector('td.align-right.nowrap');
 
     const teamNameElements = await page.$$('#league-chemp tbody tr td:nth-child(2) a');
     const xGElements = await page.$$('td.align-right.nowrap');
+    
     const xG = [];
     const xGA = [];
 
     for (let index = 0; index < teamNameElements.length; index++) {
-      const teamName = await teamNameElements[index].innerText();
+      const teamName = await page.evaluate(el => el.innerText, teamNameElements[index]);
+
+      // Initialize xGParsedValue and xGAParsedValue
+      let xGParsedValue = null;
+      let xGAParsedValue = null;
 
       // Parse xG value
-      const xGRawValue = await xGElements[index * 3].innerText();
-      const xGMatch = xGRawValue.match(/(-?\d+(\.\d+)?)(?:[\+\-]\d+(\.\d+)?)?/);
-      const xGNumericValue = xGMatch ? xGMatch[1] : null;
-      const xGParsedValue = xGNumericValue ? parseFloat(xGNumericValue) : null;
+      const xGRawValueHandle = xGElements[index * 3];
+      
+      if (xGRawValueHandle) {
+        const xGRawValue = await page.evaluate(el => el.innerText, xGRawValueHandle);
+        const xGMatch = xGRawValue.match(/(-?\d+(\.\d+)?)(?:[\+\-]\d+(\.\d+)?)?/);
+        const xGNumericValue = xGMatch ? parseFloat(xGMatch[1]) : null;
+        xGParsedValue = isNaN(xGNumericValue) ? null : xGNumericValue;
+      } else {
+        console.error('Could not find xG element at index:', index * 3);
+      }
 
       // Parse xGA value
-      const xGARawValue = await xGElements[(index * 3) + 1].innerText();
-      const xGAMatch = xGARawValue.match(/(-?\d+(\.\d+)?)(?:[\+\-]\d+(\.\d+)?)?/);
-      const xGANumericValue = xGAMatch ? xGAMatch[1] : null;
-      const xGAParsedValue = xGANumericValue ? parseFloat(xGANumericValue) : null;
+      const xGARawValueHandle = xGElements[(index * 3) + 1];
+
+      if (xGARawValueHandle) {
+        const xGARawValue = await page.evaluate(el => el.innerText, xGARawValueHandle);
+        const xGAMatch = xGARawValue.match(/(-?\d+(\.\d+)?)(?:[\+\-]\d+(\.\d+)?)?/);
+        const xGANumericValue = xGAMatch ? parseFloat(xGAMatch[1]) : null;
+        xGAParsedValue = isNaN(xGANumericValue) ? null : xGANumericValue;
+      } else {
+        console.error('Could not find xGA element at index:', (index * 3) + 1);
+      }
 
       // Push an object to the xG array for each team name
       xG.push({ teamName, xG: xGParsedValue });
@@ -51,11 +68,19 @@ async function scrapeData() {
       xGA.push({ teamName, xGA: xGAParsedValue });
     }
 
-    return { xG, xGA };
+    const finalOutput = xG.map((team, index) => ({
+      teamName: team.teamName,
+      xG: team.xG,
+      xGA: xGA[index].xGA,
+    }));
+
+    return finalOutput;
   } catch (error) {
-    console.error('Error scraping data from website:', error);
+    console.log('Error scraping data from the website:', error);
     return null;
   } finally {
     await browser.close();
   }
 }
+
+scrapeData();
